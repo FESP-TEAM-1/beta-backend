2b55b5137b927e147e5963ae81e8c5b16d1a9690
윤태현 <yoonth0919@gmail.com>
2023-12-04 12:27:26 +0900

    Fix: user-route Method 수정

--- routes/user-route.js
+++ routes/user-route.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userController = require("../controllers/user-controller");
 
 router.get("/getAllUser", userController.getAllUser);
-router.get("/getUser", userController.getUser);
-router.post("/login", userController.login);
-router.post("/logout", userController.logout);
+router.post("/getUser", userController.getUser);
+router.patch("/login", userController.login);
+router.patch("/logout", userController.logout);
 router.post("/signup", userController.signup);
 
 module.exports = router;
