4333bab37d8c1a0542086416ecbc35361aa1b686
윤태현 <yoonth0919@gmail.com>
2023-12-12 13:47:21 +0900

    Feat: show_id에 따른 전시, 공연 조회

--- controllers/show-controller.js
+++ controllers/show-controller.js
@@ -2,7 +2,7 @@ const showDB = require("../models/show-db");
 const { v4: uuid } = require("uuid");
 
 // 공연 필터링 조회
-exports.getConcerts = async (req, res) => {
+exports.getFilterConcerts = async (req, res) => {
   try {
     const category = req.query.category;
     const location = req.query.location;
@@ -10,7 +10,7 @@ exports.getConcerts = async (req, res) => {
     const end_date = req.query.end_date;
     const progress = req.query.progress;
 
-    const result = await showDB.getConcerts({
+    const result = await showDB.getFilterConcerts({
       category,
       location,
       start_date,
@@ -25,19 +25,43 @@ exports.getConcerts = async (req, res) => {
 };
 
 // 전시 필터링 조회
-exports.getExhibitions = async (req, res) => {
+exports.getFilterExhibitions = async (req, res) => {
   try {
     const location = req.query.location;
     const start_date = req.query.start_date;
     const end_date = req.query.end_date;
     const progress = req.query.progress;
 
-    const result = await showDB.getExhibitions({
+    const result = await showDB.getFilterExhibitions({
       location,
       start_date,
       end_date,
       progress,
     });
+
+    res.status(200).json({ ok: true, data: result });
+  } catch (err) {
+    res.status(500).json({ ok: false, messge: err });
+  }
+};
+
+// show_id에 따른 공연 조회
+exports.getConcert = async (req, res) => {
+  try {
+    const { show_id } = req.params;
+    const result = await showDB.getConcert({ show_id });
+
+    res.status(200).json({ ok: true, data: result });
+  } catch (err) {
+    res.status(500).json({ ok: false, messge: err });
+  }
+};
+
+// show_id에 따른 전시 조회
+exports.getExhibition = async (req, res) => {
+  try {
+    const { show_id } = req.params;
+    const result = await showDB.getExhibition({ show_id });
 
     res.status(200).json({ ok: true, data: result });
   } catch (err) {
--- models/show-db.js
+++ models/show-db.js
@@ -8,7 +8,7 @@ const commit = util.promisify(db.commit).bind(db); // 트랜잭션 커밋
 const rollback = util.promisify(db.rollback).bind(db); // 트랜잭션 롤백 (시작 지점으로)
 
 // 공연 필터링 조회
-exports.getConcerts = async ({ location, start_date, end_date, progress, category }) => {
+exports.getFilterConcerts = async ({ location, start_date, end_date, progress, category }) => {
   try {
     const { queryValue, queryParams } = filterShow("공연", { location, start_date, end_date, progress, category });
 
@@ -20,11 +20,31 @@ exports.getConcerts = async ({ location, start_date, end_date, progress, categor
 };
 
 // 전시 필터링 조회
-exports.getExhibitions = async ({ location, start_date, end_date, progress }) => {
+exports.getFilterExhibitions = async ({ location, start_date, end_date, progress }) => {
   try {
     const { queryValue, queryParams } = filterShow("전시", { location, start_date, end_date, progress });
 
     const result = await query(queryValue, queryParams);
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+// show_id에 따른 공연 조회
+exports.getConcert = async ({ show_id }) => {
+  try {
+    const result = await query(`SELECT * FROM showing WHERE show_type = '공연' AND id = ?`, [show_id]);
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+
+// show_id에 따른 전시 조회
+exports.getExhibition = async ({ show_id }) => {
+  try {
+    const result = await query(`SELECT * FROM showing WHERE show_type = '전시' AND id = ?`, [show_id]);
     return result;
   } catch (err) {
     throw err;
