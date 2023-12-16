2dbdc06feba1ce5d8138a6aae9729a44ec991cdd
윤태현 <yoonth0919@gmail.com>
2023-12-17 04:27:02 +0900

    Feat: 관리자 회원가입 메일 전송 및 인증 코드 API 추가 (#2)

--- controllers/email-controller.js
+++ controllers/email-controller.js
@@ -1,9 +1,10 @@
 const { send_main_func } = require("../utils/emailSendUtil");
 const emailDB = require("../models/email-db");
+const axios = require("axios");
+const getKoreaTime = require("../utils/getKoreaTime"); // 오늘 날짜를 가져오는 함수
 
 exports.sendEmail = async (req, res) => {
   const { user_email } = req.body;
-
   try {
     const isEmail = await emailDB.getMemberEmail(user_email);
     if (isEmail.length > 0) {
@@ -20,7 +21,7 @@ exports.sendEmail = async (req, res) => {
       .padStart(6, "0");
 
     await send_main_func({ to: user_email, VERIFICATION_CODE });
-    await emailDB.insertEmailCode(user_email, VERIFICATION_CODE);
+    await emailDB.insertEmailCode(user_email, VERIFICATION_CODE, "true");
     res.status(200).json({
       ok: true,
       data: "이메일 전송 완료",
@@ -35,6 +36,46 @@ exports.sendEmail = async (req, res) => {
   }
 };
 
+exports.sendUnivEmail = async (req, res) => {
+  const { user_email, univName } = req.body;
+  const key = process.env.UNIVCERT_API_KEY;
+  const univ_check = true;
+
+  try {
+    const response = await axios.post("https://univcert.com/api/v1/certify", {
+      key: key,
+      email: user_email,
+      univName: univName,
+      univ_check: univ_check,
+    });
+
+    if (response.data.success === false) {
+      await emailDB.insertEmailCode(user_email, null, response.data.message);
+      res.status(400).json({
+        ok: false,
+        message: response.data.message,
+      });
+      return;
+    }
+
+    await emailDB.insertEmailCode(user_email, null, "true");
+    res.status(200).json({
+      ok: true,
+      data: "전송 완료",
+    });
+  } catch (error) {
+    const response = error.response.data;
+    const code = response.code;
+    const message = response.message;
+    await emailDB.insertEmailCode(user_email, null, message);
+    res.status(code).json({
+      ok: false,
+      message: message,
+    });
+    return;
+  }
+};
+
 exports.verifyCode = async (req, res) => {
   const { user_email, code } = req.body;
 
@@ -43,6 +84,7 @@ exports.verifyCode = async (req, res) => {
     const result = await emailDB.getCertCode(user_email);
     const getCode = result[0].code;
     const getTime = result[0].created_at;
+    const getId = result[0].id;
 
     const diffTime = new Date(nowTime.getTime() - getTime.getTime()).getMinutes();
     if (diffTime >= 3) {
@@ -57,6 +99,7 @@ exports.verifyCode = async (req, res) => {
     const codeDecodedString = new TextDecoder("utf-8").decode(codeBufferData);
 
     if (code === codeDecodedString) {
+      await emailDB.updateCertCode(getId, getKoreaTime());
       res.status(200).json({
         ok: true,
         data: "인증 성공",
@@ -73,6 +116,56 @@ exports.verifyCode = async (req, res) => {
     res.status(500).json({
       ok: false,
       message: err.message,
+    });
+    return;
+  }
+};
+
+exports.verifyUnivCode = async (req, res) => {
+  const { user_email, code, univName } = req.body;
+
+  try {
+    const nowTime = new Date();
+    const result = await emailDB.getCertCode(user_email);
+    const getTime = result[0].created_at;
+    const getId = result[0].id;
+
+    const diffTime = new Date(nowTime.getTime() - getTime.getTime()).getMinutes();
+    if (diffTime >= 3) {
+      res.status(400).json({
+        ok: false,
+        message: "인증 시간이 초과되었습니다.",
+      });
+      return;
+    }
+
+    const response = await axios.post("https://univcert.com/api/v1/certifycode", {
+      key: key,
+      email: user_email,
+      univName: univName,
+      code: code,
+    });
+
+    if (response.data.success === false) {
+      res.status(400).json({
+        ok: false,
+        message: response.data.message,
+      });
+      return;
+    }
+
+    await emailDB.updateCertCode(getId, getKoreaTime());
+    res.status(200).json({
+      ok: true,
+      data: "인증 완료",
+    });
+  } catch (err) {
+    const response = err.response.data;
+    const code = response.code;
+    const message = response.message;
+    res.status(code).json({
+      ok: false,
+      message: message,
     });
     return;
   }
