e714b806355fccb3039c360fe0fa0273107ddd6f
윤태현 <yoonth0919@gmail.com>
2023-12-17 22:41:38 +0900

    Refactor: auth-middleware 토큰 검증 및 login_id 뽑아내기 (#2)

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,11 +1,20 @@
 const jwt = require("../utils/jwt-util");
 
-const storyAuthenticate = (req, res, next) => {
-  if (req.cookies.accessToken && jwt.verifyToken(req.cookies.accessToken)) {
-    next();
+const userAuthenticate = (req, res, next) => {
+  const accessToken = req.cookies.accessToken;
+
+  if (accessToken) {
+    const verifyToken = jwt.verifyToken(accessToken);
+    if (verifyToken) {
+      req.login_id = verifyToken.login_id;
+      req.user_role = verifyToken.user_role;
+      next();
+    } else {
+      res.status(401).json({ ok: false, message: "로그인 해주시기 바랍니다." });
+    }
   } else {
     res.status(401).json({ ok: false, message: "로그인 해주시기 바랍니다." });
   }
 };
 
-module.exports = { storyAuthenticate };
+module.exports = { userAuthenticate };
