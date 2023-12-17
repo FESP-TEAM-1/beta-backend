c207e86297d5c622f42a3e9b20d843cb0d54011a
윤태현 <yoonth0919@gmail.com>
2023-12-17 20:34:54 +0900

    Feat: 좋아요, 댓글 API 기능 추가 (#5)

--- routes/show-route.js
+++ routes/show-route.js
@@ -6,7 +6,14 @@ const { showAuthenticate } = require("../middleware/auth-middleware");
 router.get("/concert", showController.getFilterConcerts);
 router.get("/exhibition", showController.getFilterExhibitions);
 router.get("/detail/:show_id", showController.getShow);
+router.get("/detail/:show_id/review", showController.getShowReview);
 router.get("/show/reservation/:show_id", showController.getShowReservation);
 router.get("/show/upload", showAuthenticate, showController.uploadShow);
+router.get("/show/upload", showAuthenticate, showController.uploadShow);
+router.post("/show/like-add", showController.addLike);
+router.delete("/show/like-delete", showController.deleteLike);
+router.post("/show/review-add", showController.addReview);
+router.patch("/show/review-modify", showController.modifyReview);
+router.delete("/show/review-delete", showController.deleteReview);
 
 module.exports = router;
