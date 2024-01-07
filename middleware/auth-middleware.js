92b16a872549683a2dcd21e0cb9e4f4fe0e43e4f
윤태현 <yoonth0919@gmail.com>
2024-01-07 15:58:42 +0900

    Refactor: auth-middleware refresh 토큰 검증 기능 추가 및 코드 구조화 변경 (#9)

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,54 +1,62 @@
 const jwt = require("../utils/jwt-util");
+const userDB = require("../models/user-db");
 
-const adminAuthenticate = (req, res, next) => {
-  if (req.cookies.accessToken) {
-    const decoded = jwt.verifyToken(req.cookies.accessToken);
-    if (decoded.user_role === "admin") {
-      req.login_id = decoded.login_id;
-      req.user_role = decoded.user_role;
-      next();
-    } else {
-      res.status(401).json({ ok: false, message: "관리자로 로그인 해주시기 바랍니다." });
+// 공통된 토큰 처리 로직
+const authenticateToken = async (req, res, expectedRole) => {
+  const accessToken = req.cookies.accessToken;
+  let payload = accessToken ? jwt.verifyToken(accessToken) : null;
+  if (!payload) {
+    const refreshToken = req.cookies.refreshToken;
+    payload = refreshToken ? jwt.verifyToken(refreshToken) : null;
+    if (payload && (!expectedRole || payload.user_role === expectedRole)) {
+      const user = await userDB.getMember(payload.login_id);
+      const userInfo = {
+        login_id: user[0].login_id,
+        user_name: user[0].user_name,
+        user_role: user[0].user_role,
+      };
+      const newAccessToken = jwt.generateAccessToken(userInfo);
+      res.cookie("accessToken", newAccessToken, {
+        // domain: ".beta-beta.net", // 배포 시 주석 해제
+        path: "/",
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
+      });
+      console.log("accessToken 재발급");
     }
+  }
+
+  if (payload && (!expectedRole || payload.user_role === expectedRole)) {
+    req.login_id = payload.login_id;
+    req.user_role = payload.user_role;
+    return true;
+  }
+  return false;
+};
+
+// 관리자 권한 토큰 검증
+const adminAuthenticate = async (req, res, next) => {
+  if (await authenticateToken(req, res, "admin")) {
+    next();
   } else {
-    // 인증 실패
     res.status(401).json({ ok: false, message: "관리자로 로그인 해주시기 바랍니다." });
   }
 };
 
-const userAuthenticate = (req, res, next) => {
-  const accessToken = req.cookies.accessToken;
-
-  if (accessToken) {
-    const verifyToken = jwt.verifyToken(accessToken);
-    if (verifyToken) {
-      req.login_id = verifyToken.login_id;
-      req.user_role = verifyToken.user_role;
-      next();
-    } else {
-      res.status(401).json({ ok: false, message: "로그인 해주시기 바랍니다." });
-    }
+// 일반 유저 권한 토큰 검증
+const userAuthenticate = async (req, res, next) => {
+  if (await authenticateToken(req, res)) {
+    next();
   } else {
-    res.status(401).json({ ok: false, message: "로그인 해주시기 바랍니다." });
+    res.status(401).json({ ok: false, message: "로그인 후 이용해주세요." });
   }
 };
 
-const supervisorAuthenticate = (req, res, next) => {
-  const accessToken = req.cookies.accessToken;
-
-  if (accessToken) {
-    const verifyToken = jwt.verifyToken(accessToken);
-    if (verifyToken) {
-      if (verifyToken.user_role === "supervisor") {
-        req.login_id = verifyToken.login_id;
-        req.user_role = verifyToken.user_role;
-        next();
-      } else {
-        res.status(401).json({ ok: false, message: "접근할 수 없습니다." });
-      }
-    } else {
-      res.status(401).json({ ok: false, message: "접근할 수 없습니다." });
-    }
+// 슈퍼바이저 권한 토큰 검증
+const supervisorAuthenticate = async (req, res, next) => {
+  if (await authenticateToken(req, res, "supervisor")) {
+    next();
   } else {
     res.status(401).json({ ok: false, message: "접근할 수 없습니다." });
   }
